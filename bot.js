6f61360d771d10325cb0e1d19eded6335285ded7
XivoLol <39168262+XivoLol@users.noreply.github.com>
2018-11-02 16:14:57 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,39 @@
 const Discord = require('discord.js');
-const Rocket = new Discord.Client();
-const prefix = "x!";
-Rocket.on('message', message => { //bc
-        if(!message.channel.guild) return;
-    if(message.content.startsWith('x!bc')) {
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-    if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "XivoBot";
-    let request = `Requested By ${message.author.username}`;
-    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-    msg.react('✅')
-    .then(() => msg.react('❌'))
-    .then(() =>msg.react('✅'))
+const client = new Discord.Client();
+let timer;
 
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-    reaction1.on("collect", r => {
-    message.channel.send(`**☑ | تم ارسال البرودكاست لـ ${message.guild.members.size} عضو**`).then(m => m.delete(5000));
-    message.guild.members.forEach(m => {
-    var bc = new
-    Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setTitle('Broadcast')
-    .addField('Server', message.guild.name)
-    .addField('Sender', message.author.username)
-    .addField('Message', args)
-    .setImage("https://cdn.discordapp.com/attachments/411527218225741836/457933565930504195/broadcast-icon-vector-graphics-eps-58991.png")
-    .setThumbnail(message.author.avatarURL)
-    .setFooter(copy, Rocket.user.avatarURL);
-    m.send({ embed: bc })
-    msg.delete();
-    })
-    })
-    reaction2.on("collect", r => {
-    message.channel.send(`**تم الغاء البرودكاست.**`).then(m => m.delete(5000));
-    msg.delete();
-    })
-    })
-    }
-    });
+let d5loa = [];
+
+console.log("BOT ONLINE");
+
+client.on("guildMemberAdd", member => {
+
+       if(member.guild.id === '466760649632907266') {
+           d5loa.push(member.user.id)
+      }
+
+    if(d5loa.includes(member.user.id)) return;
+
+
+    timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
+  let words = [`**Welcome To Panorama Server   (:   :dancer::skin-tone-3: 
+
+نورنا ي قلبي   :fire: :heart: 
+ 
+
+https://discord.gg/HYXE6E**`,`**أهلآ وٰ سهلآ فيك يسعدنآ تواجدك وٰ نتمنى لك أجمل ‏الأوقات في السيرڤر‏ ✯♔
+-
+سيزداد سيرفرنا بالجّمال عند أنضمامك لقروبنـا حيـاك :heart:️. 
+
+https://discord.gg/HYXE6E**`,`**سلام :hugging:
+اتمنا تنوورنا بوجودك ي حلو ⚘:heart:
+والله يسعدك :heart:
+https://discord.gg/HYXE6E**`]
+  setTimeout(() =>{
+  member.createDM().then(function (channel) {
+  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+}).catch(console.error)
+}, timer)
+})
     
-    Rocket.login(process.env.BOT_TOKEN);
+    client.login(process.env.BOT_TOKEN);
